12f4ca3a4638505e750aee6dd73e0a06f5bfdd09
Anup Das <anup.das17@yahoo.com>
2022-01-17 13:42:28 +0530

    Adding user response & order

---
+++ coffee_machine/main.py
@@ -0,0 +1,40 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+FLAG_ON = True
+
+while FLAG_ON:
+    user_response = input("What would you like? (espresso/latte/cappuccino)").lower()
+    if user_response == "off":
+        FLAG_ON = False
+    elif user_response == "report":
+        pass
